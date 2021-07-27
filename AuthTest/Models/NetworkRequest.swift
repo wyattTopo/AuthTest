d0cc82cdb5078895b28d339842fb12147e79f5f5
Wyatt Eberspacher <wyatt@topologyeyewear.com>
2021-07-27 14:35:02 -0700

    OpenID login

---
+++ AuthTest/Models/NetworkRequest.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkRequest.swift
+//  AuthTest
+//
+//  Created by Wyatt Eberspacher on 7/27/21.
+//
+
+import Foundation
+import AuthenticationServices
+
+struct NetworkRequest {
+  // MARK: Private Constants
+  static let callbackURLScheme = "authhub"
+  static let clientID = "Iv1.0a4e211ef4ed245b"
+  static let clientSecret = "186b1751cb41bccc7f9d8fbcf50ca07f6485e6e7"
+  
+  static var signInURL: URL? {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = "https"
+    urlComponents.host = "github.com"
+    urlComponents.path = "/login/oauth/authorize"
+    urlComponents.queryItems = [URLQueryItem(name: "client_id", value: NetworkRequest.clientID)]
+    return urlComponents.url
+  }
+  
+  static func signInSession(completion: @escaping ASWebAuthenticationSession.CompletionHandler) -> ASWebAuthenticationSession? {
+    guard let signInURL = signInURL else { return nil }
+    return ASWebAuthenticationSession(url: signInURL,
+                                      callbackURLScheme: callbackURLScheme,
+                                      completionHandler: completion)
+  }
+}
--- AuthTest/ViewModels/HomeViewModel.swift
+++ AuthTest/ViewModels/HomeViewModel.swift
@@ -6,11 +6,33 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
-class HomeViewModel: ObservableObject, HomeViewDelegate {
+class HomeViewModel: NSObject, ObservableObject, HomeViewDelegate {
   @Published var doShowLoggedIn = false
   
   func handleSignInTap() {
-    doShowLoggedIn = true
+    let authSession = NetworkRequest.signInSession { [weak self] (url, error) in
+      guard let self = self,
+            error == nil,
+            let callbackURL = url
+      else { return }
+      print("successful login with callback URL: \(callbackURL)")
+      self.doShowLoggedIn = true
+    }
+    authSession?.presentationContextProvider = self
+    
+    guard authSession != nil,
+          authSession!.start() else {
+      print("Failed to start")
+      return
+    }
+  }
+}
+
+extension HomeViewModel: ASWebAuthenticationPresentationContextProviding {
+  func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+    let window = UIApplication.shared.windows.first { $0.isKeyWindow }
+    return window ?? ASPresentationAnchor()
   }
 }
